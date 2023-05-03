0a258daaa356a59843b9539111e1fc10bbab5995
hhiirono <illunasion@gmail.com>
2023-05-04 02:04:53 +0900

    InGameDataManager.cs수정

--- Assets/Scripts/Managers/InGameDataManager.cs
+++ Assets/Scripts/Managers/InGameDataManager.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 /// 소위.....짬통 
 /// 중앙 통제는 필요한데 애매한 애들 다 여기로 모아서 처리하자
 /// </summary>
-public class InGameDataManager 
+public class InGameDataManager
 {
     #region JsonData
     ScenarioHandler _scenarioHandler = new ScenarioHandler();       //자료구조
@@ -15,28 +15,22 @@ public class InGameDataManager
 
     #endregion
 
-    #region JsonData
-    NormalQuestHandler _normalquestHandler = new NormalQuestHandler();       //자료구조
-    public NormalQuestHandler NormalQuestHandler { get { return _normalquestHandler; } }       //자료구조
-
-    #endregion
-
     // Start is called before the first frame update
+    GameObject _player;
+    public GameObject Player { get { return _player; } }
     public void init()
     {
         //그냥....시작했다는 의미로 한번 넣어본
-        GameManager.ResourceManager.Instantiate("Player");
+        _player = GameManager.ResourceManager.Instantiate("Player");
         _scenarioHandler = Util.ParseJson<ScenarioHandler>();       //Json data를 자료구조로 가지고 오기
 
         Debug.Log(_scenarioHandler[$"{1}_{0}"].Dialogue);           //
 
-        _normalquestHandler = Util.ParseJson<NormalQuestHandler>();
-        Debug.Log(_normalquestHandler[1].Skip);
     }
 
     // Update is called once per frame
     public void Clear()
     {
-        
+
     }
 }
