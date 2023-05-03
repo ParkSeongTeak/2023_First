9a2123a3958c8f6dae6a9942b91df54804f32056
hhiirono <130774286+hhiirono@users.noreply.github.com>
2023-05-04 01:44:53 +0900

    Add files via upload

---
+++ Assets/Scripts/UI/SkipBtn.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SkipBtn : EventTriggerEX
+{
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        init();
+    }
+    protected override void
+        OnPointerClick(PointerEventData data)
+    {
+        Debug.Log("SkipBtn´­¸²");
+        GameManager.InGameDataManager.Player.transform.position += new Vector3(1, 0, 0);
+    }
+            
+         
+
+        
+
+
+
+    // Update is called once per frame
+    
+        
+    
+}
