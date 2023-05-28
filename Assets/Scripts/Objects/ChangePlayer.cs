6d2367522bc84c97cface422b9d7a00b5bc2a60d
hhiirono <illunasion@gmail.com>
2023-05-28 15:25:26 +0900

    y축 좌표에 따른 플레이어 이미지변화

---
+++ Assets/Scripts/Objects/ChangePlayer.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangePlayer: MonoBehaviour
+{
+
+    public float thresholdHeight = -6;
+    public Sprite imageUnderThreshold;
+    public Sprite defaultImage;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < thresholdHeight)
+        {
+            spriteRenderer.sprite = imageUnderThreshold;
+        }
+        else
+        {
+            spriteRenderer.sprite = defaultImage;
+        }
+    }
+}
+
