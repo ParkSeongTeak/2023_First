488db7695d64f2fa84273f9261bca095209fe9fe
ParkSungTaek <56757197+ParkSungTaek@users.noreply.github.com>
2023-08-01 20:40:15 +0900

    작업중중

--- Assets/Scripts/Objects/Tiles/FlowerBudTile.cs
+++ Assets/Scripts/Objects/Tiles/FlowerBudTile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,15 +10,20 @@ public class FlowerBudTile : Tile
     public Define.FlowerTypes MyFlowerType { get; set; }
     public Define.CosmosFlower FlowerJumpType { get; set; }
     
+    Animator _animator;
+    AnimationClip _animationClip;
 
     public override void Init()
     {
-
+        _animator = GetComponent<Animator>();
         MyFlowerType = TileController.Instance.SetFlowerType(); //���߿� 3�� �� �������� ���� ���� �ڵ�� �����ؾ���
 
         FlowerJumpType = TileController.Instance.SetCosmosFlowerType();
         transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.CosmosFlowerSprites[(int)FlowerJumpType];
         JumpLeft = (int)FlowerJumpType;
+        string AnimName = $"{Enum.GetName(typeof(Define.FlowerTypes), MyFlowerType)}{JumpLeft}";
+        Debug.Log(AnimName);
+
     }
     public override void JumpOnMe()
     {
@@ -25,15 +31,33 @@ public class FlowerBudTile : Tile
         //base.JumpOnMe();
         if (JumpLeft >= 0)
         {
-            JumpLeft--;
-            if (JumpLeft > -1)
+            
+            if (JumpLeft > 0)
             {
-                transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.CosmosFlowerSprites[JumpLeft];
+                //transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.CosmosFlowerSprites[JumpLeft];
+
+                string AnimName = $"{Enum.GetName(typeof(Define.FlowerTypes), MyFlowerType)}{JumpLeft}";
+                Debug.Log(AnimName);
+                _animationClip = GameManager.ResourceManager.Load<AnimationClip>($"Animation/FlowerBudAnims/{AnimName}");
+            
+                if(_animationClip != null) 
+                {
+                    _animator.Play(_animationClip.name);
+                }
+                else
+                {
+                    transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.CosmosFlowerSprites[JumpLeft];
+
+                }
             }
-            if (JumpLeft == -1)
+
+            JumpLeft--;
+            
+            if (JumpLeft == 0)
             {
-                transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.FlowerSprites[(int)MyFlowerType];
-                GameManager.InGameDataManager.NowState.BloomCnt++;
+                //transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.FlowerSprites[(int)MyFlowerType];
+                //GameManager.InGameDataManager.NowState.BloomCnt++;
+                StartCoroutine(Bloom());
             }
         }
         else
@@ -51,6 +75,14 @@ public class FlowerBudTile : Tile
 
     public override void AllBloom()
     {
+        transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.FlowerSprites[(int)MyFlowerType];
+        GameManager.InGameDataManager.NowState.BloomCnt++;
+    }
+
+    IEnumerator Bloom()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         transform.GetComponent<SpriteRenderer>().sprite = TileController.Instance.FlowerSprites[(int)MyFlowerType];
         GameManager.InGameDataManager.NowState.BloomCnt++;
     }
