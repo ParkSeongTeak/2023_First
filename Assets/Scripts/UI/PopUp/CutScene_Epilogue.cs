6a0427a6dd5a081ca9fe18d21abb403cefe43e8c
hhiirono <illunasion@gmail.com>
2023-08-20 16:58:18 +0900

    Update CutScene_Epilogue.cs

--- Assets/Scripts/UI/PopUp/CutScene_Epilogue.cs
+++ Assets/Scripts/UI/PopUp/CutScene_Epilogue.cs
@@ -97,6 +97,8 @@ public class CutScene_Epilogue : UI_PopUp
             else
             {
                 GetImage((int)CutScene).gameObject.SetActive(true);
+                PlaySFXForImage(CutScene);//수정한 부분 조심하자!
+
                 if (CutScene == Images.CutScene_epilogue2_1)
                 {
                     for (int i = 0; i < 3; i++)
@@ -110,4 +112,61 @@ public class CutScene_Epilogue : UI_PopUp
         }
         
     }
+    //여기까지 원본
+
+
+
+    void Btn_CutScene_SFX(PointerEventData evt)
+    {
+        StopCoroutine(Coroutine);
+
+        if (CutScene == Images.BG)
+        {
+            GameManager.InGameDataManager.NeedToShowCutScene_prologue = false;
+            ClosePopupUI();
+            GameManager.SoundManager.AudioSources[(int)Define.Sounds.BGM].mute = false;
+            return;
+        }
+
+        // 이미지 변경
+        GetImage((int)CutScene).gameObject.SetActive(true);
+
+        // SFX 재생
+        PlaySFXForImage(CutScene);
+
+        if (CutScene == Images.CutScene_epilogue2_1)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                GetImage(i)?.gameObject.SetActive(false);
+            }
+        }
+        
+
+        CutScene++;
+        Coroutine = StartCoroutine(Btn_CutScene_Epilogue_Auto());
+    }
+
+    // (기존 코드 생략)
+
+    // SFX 재생 로직 추가
+    void PlaySFXForImage(Images image)
+    {
+        switch (image)
+        {
+      
+            case Images.CutScene_epilogue2_2:
+                GameManager.SoundManager.Play(Define.SFX.PaperOnFloor);
+                break;
+
+            default:
+                // 기본 동작을 정의하거나 특별한 SFX가 없을 경우 아무 동작도 수행하지 않음
+                break;
+        }
+    }
 }
+
+
+
+
+
