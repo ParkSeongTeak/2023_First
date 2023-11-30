bf69bd06e949702b47d87eb4ed15b6edeacd6f68
ParkSungTaek <sbag8487@gmail.com>
2023-11-30 12:13:33 +0900

    Update BookSelect.cs

--- Assets/Scripts/UI/FlowersBookState/BookSelect.cs
+++ Assets/Scripts/UI/FlowersBookState/BookSelect.cs
@@ -18,16 +18,11 @@ public class BookSelect : BookState
             FlowersBookUI.EnqueueSelectQueue(Button_.GetFlowerType());
 
             Color color = new Color(1, 1, 1, 0.5f);
-            //Color color = new Color(0.1f, 0.1f, 0.1f, 1f);
 
             Define.FlowerBookIMG tmp = (Define.FlowerBookIMG)Enum.Parse(typeof(FlowerTypes), Button_.GetType().Name);
             
             evt.selectedObject.GetComponent<Image>().sprite = GameManager.ResourceManager.Load<Sprite>($"Sprites/OutLine/{Enum.GetName(typeof(Define.FlowerBookIMG), tmp)}");
             
-
-            //evt.selectedObject.GetComponent<Image>().color = color;
-
-
         }
         else
         {
